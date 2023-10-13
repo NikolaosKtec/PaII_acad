43959f6c95a0529b3fd643bfc530a26715b1af9d
nikolaos_ferreira_daskalakis <nikolaosf1daskalakis@gmail.com>
2023-10-13 11:27:44 -0300

    add_front_files

---
+++ proj_acad_paII/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+// import Home from './components/home/Home.vue';
+
+</script>
+
+<template >
+  <!-- <Home/> -->
+  <RouterView />
+  <!-- <RouterLink/> -->
+</template>
+
+<style src="./App.css" scoped>
+
+</style>
---
+++ proj_acad_paII/src/components/login/Login.vue
@@ -0,0 +1,55 @@
+<script>
+import { ref } from 'vue'
+export default {
+   data(){
+    const text = ref('ola')
+    
+
+    function doLogin(){
+        alert(text.value)
+    }
+    
+
+    
+    // expose the ref to the template
+    return {
+      text,doLogin
+    }
+   }
+}
+
+</script>
+
+<template>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      
+        <h1 class="m-auto">Logue para acessar painel</h1>
+      
+    </nav>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <form class="container-fluid w-50 ms-auto mt-5" >
+          <div class="mb-3 ">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input   type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+  </div>
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button v-on:click="doLogin" type="submit" class="btn btn-primary">Entrar</button>
+</form>
+
+
+</template>
+
+<style>
+    
+</style>
