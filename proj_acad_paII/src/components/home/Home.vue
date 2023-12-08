e6a30dd5dc5e9f49be9a99ad4656db978043197e
nikolaos_ferreira_daskalakis <nikolaosf1daskalakis@gmail.com>
2023-12-08 09:21:03 -0300

    updates

--- proj_acad_paII/src/components/home/Home.vue
+++ proj_acad_paII/src/components/home/Home.vue
@@ -1,6 +1,35 @@
 <script>
 import { RouterLink, RouterView } from 'vue-router'
+export default {
+        data () {
+            return {};
+        },
+        // load() {
+        //      let scriptElI = document.createElement('script');
+        //      let scriptElII = document.createElement('script');
+        //      let scriptElIII = document.createElement('script');
+        //      let scriptElIV = document.createElement('script');
+        //      let scriptElV = document.createElement('script');
+        //      let scriptElVI = document.createElement('script');
+        //      let scriptElVII = document.createElement('script');
 
+        //      scriptElI.setAttribute('src', 'assets/vendor/purecounter/purecounter_vanilla.js');
+        //      scriptElII.setAttribute('src', 'assets/vendor/aos/aos.js');
+        //      scriptElIII.setAttribute('src', 'assets/vendor/bootstrap/js/bootstrap.bundle.min.js');
+        //      scriptElIV.setAttribute('src', 'assets/vendor/glightbox/js/glightbox.min.js');
+        //      scriptElV.setAttribute('src', 'assets/vendor/isotope-layout/isotope.pkgd.min.js');
+        //      scriptElVI.setAttribute('src', 'assets/vendor/swiper/swiper-bundle.min.js');
+        //      scriptElVII.setAttribute('src', 'assets/vendor/php-email-form/validate.js');
+             
+        //      this.$els.scriptHolderI.appendChild(scriptElI);
+        //      this.$els.scriptHolderI.appendChild(scriptElII);
+        //      this.$els.scriptHolderI.appendChild(scriptElIII);
+        //      this.$els.scriptHolderI.appendChild(scriptElVI);
+        //      this.$els.scriptHolderI.appendChild(scriptElV);
+        //      this.$els.scriptHolderI.appendChild(scriptElVI);
+        //      this.$els.scriptHolderI.appendChild(scriptElVII);
+        // },
+    }
 </script>
 <template src="./Home.html">
 
@@ -8,6 +37,6 @@ import { RouterLink, RouterView } from 'vue-router'
 
 </template>
 
-<style scoped src="./assets/css/unific_styles.css">
+<style scoped src="">
 
 </style>
